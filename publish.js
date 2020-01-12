c0a1e3ce78e55e024fd3f656106b4d02a4b35358
Ronnyriyana <riyanaronny@gmail.com>
2020-01-12 12:48:25 +0700

    polusiid

--- publish.js
+++ publish.js
@@ -1,12 +1,12 @@
 var mqtt = require('mqtt');
 var options={
-clientId:"mqttjs01heroku",
+clientId:"mqttjs01polusiid",
 username:"qlnzqwhk",
 password:"ojPBiWN32mq_",
 port:12241};
 
 var client = mqtt.connect("mqtt://tailor.cloudmqtt.com",options);
 client.on('connect', function () {setInterval(function() {
-	client.publish('heroku', 'Hello mqtt hola dari heroku');
+	client.publish('heroku', 'Hello mqtt hola dari polusi.id');
 	console.log('Message Sent');
 }, 5000);});
